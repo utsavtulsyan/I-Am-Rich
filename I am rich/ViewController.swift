3a04fff7ba4daae31be3d7d26a55ee02fc98e45e
Utsav Tulsyan <utsav.tulsyan@publicissapient.com>
2020-09-22 00:10:54 +0530

    Initial Commit

---
+++ I am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Utsav Tulsyan on 22/09/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
